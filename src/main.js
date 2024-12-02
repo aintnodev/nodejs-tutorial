a74bb9ea52e3c0164b009e1a2f573ba220c288b9
Ishaan Gupta <ishaan.ishu@proton.me>
2024-12-02 07:41:49 +0530

    Get started with MongoDB

--- src/main.js
+++ src/main.js
@@ -16,12 +16,20 @@
 // console.log(uuidv4());
 
 const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+const Customer = require("./models/customers");
+
 const app = express();
+
+dotenv.config();
+mongoose.set("strictQuery", false);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 const customers = [
   {
@@ -51,23 +59,111 @@ const customers = [
   },
 ];
 
-app.get("/", (req, res) => {
-  res.send("This is a GET request.");
+const customer = new Customer({
+  fname: "another",
+  lname: "customer",
+  reality: "nothing",
 });
 
-app.get("/api/customers", (req, res) => {
-  res.send({ customers: customers });
+// customer.save();
+
+app.get("/", (req, res) => {
+  res.send(customer);
+});
+
+app.get("/api/customers", async (req, res) => {
+  // console.log(await mongoose.connection.db.listCollections().toArray());
+  try {
+    const result = await Customer.find();
+    res.json({ customers: result });
+  } catch (e) {
+    res.status(500).json({ error: e.message });
+  }
+});
+
+app.get("/api/customers/:id", async (req, res) => {
+  console.log({
+    requestParams: req.params,
+    requestQuery: req.query,
+  });
+  try {
+    const { id: customerId } = req.params;
+    console.log(customerId);
+    const customer = await Customer.findById(customerId);
+    if (!customer) {
+      res.status(404).json({ error: "user not found" });
+    }
+    res.json({ customer });
+  } catch (e) {
+    res.status(500).json({ error: "something went wrong" });
+  }
+});
+
+app.put("/api/customers/:id", async (req, res) => {
+  try {
+    const customerId = req.params.id;
+    const result = await Customer.replaceOne({ _id: customerId });
+    console.log(result);
+    res.json({ updatedCount: result.modifiedCount });
+  } catch (e) {
+    res.status(500).json({ error: "something went wrong" });
+  }
+});
+
+app.delete("/api/customers/:id", async (req, res) => {
+  try {
+    const customerId = req.params.id;
+    const result = await Customer.deleteOne({ _id: customerId });
+    res.json({ deletedCount: result.deletedCount });
+  } catch (e) {
+    res.status(505).json({ error: "something went wrong" });
+  }
 });
 
 app.post("/", (req, res) => {
-  res.send("This is a POST request.");
+  res.send(customer);
 });
 
-app.post("/api/customers", (req, res) => {
+app.post("/api/customers", async (req, res) => {
   console.log(req.body);
-  res.send(req.body);
+  const customer = new Customer(req.body);
+  try {
+    await customer.save();
+    res.status(201).json({ customer });
+  } catch (e) {
+    res.status(400).json({ error: e.message });
+  }
 });
 
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}.`);
-});
+const start = async () => {
+  try {
+    await mongoose.connect(process.env.CONNECTION);
+
+    app.listen(PORT, () => {
+      console.log(`App listening on port ${PORT}.`);
+    });
+  } catch (e) {
+    console.log(e.message);
+  }
+};
+
+start();
+
+// class Person {
+//   constructor(fname, age) {
+//     this.fname = fname;
+//     this.age = age;
+//   }
+
+//   name() {
+//     console.log(this.fname);
+//   }
+
+//   pAge() {
+//     console.log(this.age);
+//   }
+// }
+
+// const ishu = new Person("ishu", 7);
+// ishu.name();
+// ishu.pAge();
